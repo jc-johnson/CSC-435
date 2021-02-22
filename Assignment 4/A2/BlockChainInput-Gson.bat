27c6abdf093a33548bc3e3be32f89e0996b6158f
Jordan C. Johnson <jc-johnson@users.noreply.github.com>
2021-02-22 12:12:51 -0600

    Able to read additional test value from text files

---
+++ Assignment 4/A2/BlockChainInput-Gson.bat
@@ -0,0 +1,5 @@
+javac -cp "gson-2.8.2.jar" BlockChainInput.java
+java -cp ".;gson-2.8.2.jar" BlockChainInput
+java -cp ".;gson-2.8.2.jar" BlockChainInput 1
+java -cp ".;gson-2.8.2.jar" BlockChainInput 2
+pause
